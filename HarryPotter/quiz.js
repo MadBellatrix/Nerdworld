d16db2803d3c339e2bf0dbddad459a1ab618207e
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-05-11 17:53:54 +0200

    doppelte draw function gelöscht .  kleien Code korrektur damit nachher immer die aktuelle frage angezeigt wird

--- HarryPotter/quiz.js
+++ HarryPotter/quiz.js
@@ -40,11 +40,11 @@ function drawQuestion() {
   
       optionBoxes.push({ x, y, width, height, index: i });
     });
-  }
+}
 
-  drawQuestion();
+drawQuestion();
 
-  canvas.addEventListener("click", handleClick);
+canvas.addEventListener("click", handleClick);
 
 function handleClick(e) {
   const rect = canvas.getBoundingClientRect();
@@ -54,31 +54,14 @@ function handleClick(e) {
   for (let i = 0; i < optionBoxes.length; i++) {
     const box = optionBoxes[i];
     if (x >= box.x && x <= box.x + box.width && y >= box.y && y <= box.y + box.height) {
-      const isCorrect = i === questions[0].correct;
+      const isCorrect = i === questions[currentQuestion].correct;
+
+      if (isCorrect) {
+        score++;
+      }
+
       ctx.fillStyle = isCorrect ? "green" : "red";
       ctx.fillText(isCorrect ? "Richtig!" : "Falsch!", 50, 450);
     }
   }
-}
-
-function drawQuestion() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const q = questions[0];
-
-  ctx.font = "28px Arial";
-  ctx.fillStyle = "black";
-  ctx.fillText(`Frage 1: ${q.question}`, 50, 80);
-
-  optionBoxes.length = 0;
-  q.options.forEach((opt, i) => {
-    const x = 50;
-    const y = 150 + i * 70;
-    const width = 700;
-    const height = 50;
-    ctx.fillStyle = "#f0f0f0";
-    ctx.fillRect(x, y, width, height);
-    ctx.fillStyle = "black";
-    ctx.fillText(opt, x + 20, y + 32);
-    optionBoxes.push({ x, y, width, height });
-  });
 }

6dbb078cdd619cdb3f516a5e5c60dc89a3cafe7c
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-05-11 15:54:40 +0200

    Fragen-array erstellt und Initialwerte eingetrageb

--- HarryPotter/quiz.js
+++ HarryPotter/quiz.js
@@ -1,2 +1,16 @@
 const canvas = document.getElementById("quizCanvas");
 const ctx = canvas.getContext("2d");
+
+const questions = [
+    {question: "Wie heißt die Schule, die Harry Potter besucht?", options: ["Beauxbatons", "Durmstrang", "Hogwarts", "Ilvermorny"], correct: 2},
+    {question: "Wie lautet Harry Potters vollständiger Name?", options: ["Harry Sirius Potter", "Harry James Potter", "Harry Albus Potter", "Harry Remus Potter"], correct: 1},
+    {question: "Welches Haus wird Harry zugeteilt?", options: ["Hufflepuff", "Ravenclaw", "Gryffindor", "Slytherin"], correct: 2},
+    {question: "Wer ist der Schulleiter in Harrys erstem Schuljahr?", options: ["Snape", "Dumbledore", "McGonagall", "Flitwick"], correct: 1}
+];
+
+let currentQuestion = 0;
+let score = 0;
+let restartButton = null;
+let feedback = null;
+let feedbackTimer = null;
+const optionBoxes = [];
